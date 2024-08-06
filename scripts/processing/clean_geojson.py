ac941ca7645c82c73cbba30af5bdbb145031b21c
Henrik <henrrb@stud.ntnu.no>
2024-08-06 03:27:43 +0200

    Add cleaning function for geojson data

--- scripts/processing/clean_geojson.py
+++ scripts/processing/clean_geojson.py
@@ -4,28 +4,63 @@ import pandas as pd
 from pprint import pprint
 
 
-def clean_geojson(city: str) -> None:
+def clean_geojson(poststed: str, city: str) -> None:
     
     # Paths
-    geojson_path = f"../data/geojson/postnummers_{city}.geojson"
-    geojson_cleaned_path = f"../data/postcodes_cleaned/postnummers_{city}.json"
+    geojson_path = f"../data/geojson/postnummers_{poststed}.geojson"
+    geojson_cleaned_path = f"../data/postcodes_cleaned/postnummers_{poststed}.json"
 
     # Load geojson
     with open(geojson_path, "r") as f:
         geojson_data = json.load(f)
     
+    # Get only necessary data
+    geojson_cleaned_data = {}
+    geojson_cleaned_data["type"] = geojson_data["postnummeromrader.postnummeromrade"]["type"]
+    geojson_cleaned_data["features"] = []
+    for i, feature in enumerate(geojson_data["postnummeromrader.postnummeromrade"]["features"]):
+        if feature["properties"]["poststed"] == city:       # Filter only features with "poststed" = city
+            
+            # Clean "poststed" names that are parsed incorrectly
+            poststed = feature["properties"]["poststed"]
+            if poststed == "KRISTIANSAND S": poststed = "KRISTIANSAND"
+            if poststed == "BODÃ˜": poststed = "BODØ"
+            if poststed == "TROMSÃ˜": poststed = "TROMSØ"
+            
+            # Append cleaned data
+            geojson_cleaned_data["features"].append({
+                "type": feature["type"],
+                "geometry": feature["geometry"],
+                "properties": {
+                    "objtype": feature["properties"]["objtype"],
+                    "postnummer": feature["properties"]["postnummer"],
+                    "poststed": poststed, 
+                }
+            })
     
-
-
+    # Save cleaned geojson
+    with open(geojson_cleaned_path, "w") as f:
+        json.dump(geojson_cleaned_data, f, indent=2)
+        
+    print(f"Geojson with {len(geojson_cleaned_data['features'])} cleaned for {city} and saved to {geojson_cleaned_path}!")
 
 def main():
     
     # Poststeder
-    cities = ["oslo", "trondelag", "bergen", "stavanger", "tromso"]
+    poststed_cities_dict = {
+        "oslo": "OSLO",
+        "drammen": "DRAMMEN",
+        "kristiansand": "KRISTIANSAND S", # "S" is added when parsing "poststed" from geojson
+        "stavanger": "STAVANGER",
+        "bergen": "BERGEN",
+        "trondelag": "TRONDHEIM",
+        "bodo": "BODÃ˜",                  # Special character when parsing "poststed" from geojson
+        "tromso": "TROMSÃ˜",                # Special character when parsing "poststed" from geojson
+    }
     
     # Clean geojson files
-    for city in cities:
-        clean_geojson(city)
+    for poststed, city in poststed_cities_dict.items():
+        clean_geojson(poststed, city)
     
 if __name__ == "__main__":
     main()
