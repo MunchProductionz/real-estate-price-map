764af974abb88e6e882147373a354d10573bdc0e
eivindnesje <eivines@stud.ntnu.no>
2024-10-18 01:25:49 +0200

    Fixed correct datafetching

--- frontend/src/services/queryClient.ts
+++ frontend/src/services/queryClient.ts
@@ -1,14 +1,21 @@
 import { QueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
+const getBaseUrl = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:5173';
+  } else {
+    return 'https://real-estate-map.eivindnesje.no';
+  }
+};
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       queryFn: async ({ queryKey }) => {
         const [url] = queryKey;
-        const { data } = await axios.get(
-          `http://localhost:5173/data/${url as string}`,
-        );
+        const baseUrl = getBaseUrl(); // Get base URL based on environment
+        const { data } = await axios.get(`${baseUrl}/data/${url as string}`);
         return data;
       },
     },
